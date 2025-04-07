6bd6f712420be6c344e3d9e30ddc341cfd2e8672
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-04-08 00:20:28 +0200

    chore(data): added projects array to use white testing the app

---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    _id: "pr_123",
+    title: "E-Learning",
+    description: "Hello there!",
+    isActive: true,
+  },
+  {
+    _id: "pr_456",
+    title: "E-Learning",
+    description: "Hello there!",
+    isActive: false,
+  },
+  {
+    _id: "pr_789",
+    title: "E-Learning",
+    description: "Hello there!",
+    isActive: true,
+  },
+  {
+    _id: "pr_111",
+    title: "E-Learning",
+    description: "Hello there!",
+    isActive: true,
+  },
+  {
+    _id: "pr_222",
+    title: "E-Learning",
+    description: "Hello there!",
+    isActive: false,
+  },
+  {
+    _id: "pr_333",
+    title: "E-Learning",
+    description: "Hello there!",
+    isActive: false,
+  },
+  {
+    _id: "pr_444",
+    title: "E-Learning",
+    description: "Hello there!",
+    isActive: true,
+  },
+];

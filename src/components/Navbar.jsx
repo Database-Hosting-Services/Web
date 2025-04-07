56ba4ebcbe154994010f4e555b10b130b1644dba
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-04-08 01:49:38 +0200

    feat(Notification UI): add the notification ui that appears when clicking the bell

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import UserInfo from "./UserInfo";
+import NotificationsContainer from "./NotificationsContainer";
 
 const Navbar = () => {
+  const [showNotifications, setShowNotifications] = useState(false);
+
+  const toggleShowNotifications = () => {
+    setShowNotifications((prevShowNotifications) => !prevShowNotifications);
+  };
+
   return (
     <nav className="flex items-center bg-[#06071A] p-7 border-gray-800 border-b-1 border-l-1 h-[117px] text-white">
       <h1 className="mr-auto font-bold text-4xl">Dashboard</h1>
-      <p className="mr-5">🔔</p>
+      <p className="relative mr-5">
+        <span className="cursor-pointer" onClick={toggleShowNotifications}>
+          🔔
+        </span>
+        {showNotifications && (
+          <span className="top-10 right-0 absolute">
+            <NotificationsContainer />
+          </span>
+        )}
+      </p>
       <UserInfo />
     </nav>
   );
---
+++ src/components/NotificationsContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotificationsContainer = () => {
+  return (
+    <div className="bg-secondary rounded-2xl w-[571px] h-[660px] overflow-hidden">
+      <div className="flex justify-start items-center bg-tertiary pl-7 w-full h-[100px] font-medium text-[20px]">
+        Notification
+      </div>
+    </div>
+  );
+};
+
+export default NotificationsContainer;

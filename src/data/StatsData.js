3052b10168c6c3959109a4ea6665c020020271b5
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-04-14 17:30:50 +0200

    chore(Stats data): added project stats data (icon, text)

---
+++ src/data/StatsData.js
@@ -0,0 +1,18 @@
+import databaseImg from "../assets/database.svg";
+import clockImg from "../assets/clock.svg";
+import cupImg from "../assets/cpu.svg";
+
+export default [
+  {
+    icon: databaseImg,
+    text: "Database Requests",
+  },
+  {
+    icon: clockImg,
+    text: "Requests Time",
+  },
+  {
+    icon: cupImg,
+    text: "CPU Usage",
+  },
+];

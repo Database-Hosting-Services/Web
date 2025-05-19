b47dc951f0cd2c4440c76b07ddf9726b30d937ed
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-05-19 22:35:15 +0300

    fix(typo): renamed the it from publicAxios to privateAxios

--- src/api/private.js
+++ src/api/private.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const publicAxios = axios.create({
+const privateAxios = axios.create({
   baseURL: "https://api-production-67e2.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-publicAxios.interceptors.request.use(
+privateAxios.interceptors.request.use(
   (request) => {
     return request;
   },
@@ -17,4 +17,4 @@ publicAxios.interceptors.request.use(
   },
 );
 
-export default publicAxios;
+export default privateAxios;

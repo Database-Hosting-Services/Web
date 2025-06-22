fa52171c1e5c5f10fc0373b6ea31f16ac24c9644
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-06-22 20:44:29 +0300

    refactor(Action Name): renamed reset.js -> resetOTP.js

--- src/features/auth/actions/index.js
+++ src/features/auth/actions/index.js
@@ -1,5 +1,5 @@
 export { default as verifyEmailAction } from "./verifyEmail";
-export { default as resendOTPAction } from "./resend";
+export { default as resendOTPAction } from "./resendOTP";
 export { default as signUpAction } from "./signUp";
 export { default as signInAction } from "./signIn";
 export { default as forgotPasswordAction } from "./forgotPassword";

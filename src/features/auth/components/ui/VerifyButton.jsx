fe1eef0629432244c09060c9965bfe602300abc8
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-06-23 18:21:35 +0300

    refactor (use the Form element instead of just navigating when the button is clicked

--- src/features/auth/components/ui/VerifyButton.jsx
+++ src/features/auth/components/ui/VerifyButton.jsx
@@ -1,34 +1,24 @@
-import {
-  Form,
-  useLocation,
-  useNavigate,
-  useNavigation,
-} from "react-router-dom";
+import { Form, useLocation, useNavigation } from "react-router-dom";
 
 const VerifyButton = ({ email, otpCode }) => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
-  const navigate = useNavigate();
-
   const location = useLocation();
 
   if (location.pathname === "/forgot-password/reset-otp") {
     return (
-      <div className="w-1/2">
+      <Form action="/forgot-password/reset-otp" method="post" className="w-1/2">
+        <input type="hidden" name="email" value={email} />
+        <input type="hidden" name="otpCode" value={otpCode} />
         <button
+          type="submit"
           className="bg-linear-to-t from-gradientEnd to-gradientStart hover:opacity-75 ml-2 py-2 rounded-lg w-full text-[#FFFFFF] text-lg transition-opacity duration-300 cursor-pointer"
-          onClick={() =>
-            navigate(
-              `/forgot-password/verify-and-reset-password?email=${encodeURIComponent(
-                email,
-              )}&otpCode=${otpCode}`,
-            )
-          }
+          disabled={isSubmitting}
         >
-          Proceed
+          {isSubmitting ? "Proceeding..." : "Proceed"}
         </button>
-      </div>
+      </Form>
     );
   }
 

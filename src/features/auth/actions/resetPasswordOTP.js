a241820d65bba0f12443c83edc6dfbfd9be838ea
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-06-23 18:22:15 +0300

    feat(API Actions): resetPasswordOTP.js Action DONE

--- src/features/auth/actions/resetPasswordOTP.js
+++ src/features/auth/actions/resetPasswordOTP.js
@@ -1,11 +1,13 @@
 import { redirect } from "react-router-dom";
-import { successToast } from "../../../utils/toastConfig";
 
 export default async function verifyAction({ request }) {
   const formData = await request.formData();
   const email = formData.get("email");
   const otpCode = formData.get("otpCode");
-  console.log("verifyAction", { email, otpCode });
 
-  return redirect();
+  return redirect(
+    `/forgot-password/verify-and-reset-password?email=${encodeURIComponent(
+      email,
+    )}&otpCode=${otpCode}`,
+  );
 }

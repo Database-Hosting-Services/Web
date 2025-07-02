62bebce5d7da3ae02e391511ac1b0873f8dd4e2f
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-07-02 14:40:47 +0300

    feat(API Loader): ProjectHome Loader Done (fetching the project data from the server and displaying it

--- src/pages/dashboard/ProjectHome.jsx
+++ src/pages/dashboard/ProjectHome.jsx
@@ -1,32 +1,45 @@
-import React, { useEffect } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useEffect } from "react";
+import { redirect, useLoaderData } from "react-router-dom";
 
-import { useDashboardContext } from "../../features/dashboard/store/DashboardContext";
+import { privateAxios } from "../../api";
+import { PROJECTS_ENDPOINTS } from "../../features/dashboard/api/endpoints";
+import { errorToast } from "../../utils/toastConfig";
+
 import ProjectStats from "../../features/dashboard/components/ProjectStats";
 import StatsData from "../../features/dashboard/data/StatsData";
-import projectsData from "../../features/dashboard/data/projects";
 import ConnectingProject from "../../features/dashboard/components/ConnectingProject";
+
+import { useDashboardContext } from "../../features/dashboard/store/DashboardContext";
 
 export const loader = async ({ params }) => {
   const { projectId } = params;
-  const projectTitle = projectsData.find(
-    (project) => project._id === projectId,
-  ).title;
-  return { projectId, projectTitle };
+
+  try {
+    const {
+      data: { data },
+    } = await privateAxios.get(PROJECTS_ENDPOINTS.getProject(projectId));
+
+    console.log(data);
+
+    return { projectData: { ...data, _id: data.oid, title: data.name } };
+  } catch (err) {
+    errorToast(err?.response?.data?.message || "Failed to fetch project data");
+    return redirect("/dashboard");
+  }
 };
 
 const ProjectHome = () => {
-  const { projectId, projectTitle } = useLoaderData();
+  const { projectData: fetchedProjectData } = useLoaderData();
 
-  const { updateProjectData, projectData } = useDashboardContext();
+  const { updateProjectData } = useDashboardContext();
 
   useEffect(() => {
-    updateProjectData(projectId, projectTitle);
-  }, [projectId, projectTitle]);
+    updateProjectData(fetchedProjectData._id, fetchedProjectData.title);
+  }, []);
 
   return (
     <div>
-      <h2 className="mb-15 font-bold text-4xl">{projectData.title}</h2>
+      <h2 className="mb-15 font-bold text-4xl">{fetchedProjectData.title}</h2>
       <div className="gap-2 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 mb-30">
         {StatsData.map((StatsItem) => (
           <ProjectStats

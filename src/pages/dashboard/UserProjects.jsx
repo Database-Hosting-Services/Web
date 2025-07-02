e7806b071a7e6cf115eebea2dce63f8fa505e9e7
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-07-02 14:25:35 +0300

    fix(returned projects from loader): fix the format of the projects we're getting from the loader

--- src/pages/dashboard/UserProjects.jsx
+++ src/pages/dashboard/UserProjects.jsx
@@ -1,12 +1,9 @@
-// import { useEffect } from "react";
 import {
   ProjectsSearchFilter,
   SingleProject,
 } from "../../features/dashboard/components";
-// import theProjects from "../../features/dashboard/data/projects";
 
 import orbixFloatingImg from "../../assets/orbixFloating.svg";
-// import { useDashboardContext } from "../../features/dashboard/store/DashboardContext";
 import { privateAxios } from "../../api";
 import { PROJECTS_ENDPOINTS } from "../../features/dashboard/api/endpoints";
 import { errorToast } from "../../utils/toastConfig";
@@ -16,9 +13,14 @@ export const loader = async () => {
   try {
     const { data } = await privateAxios.get(PROJECTS_ENDPOINTS.getProjects());
 
-    console.log(data);
-
-    return { projects: data.projects };
+    return {
+      projects: data.data.map((project) => ({
+        _id: project.oid,
+        title: project.name,
+        description: project.description,
+        isActive: project.status === "active",
+      })),
+    };
   } catch (err) {
     errorToast(err?.response?.data?.message || "Failed to fetch projects");
     return redirect("/signin");
@@ -27,11 +29,6 @@ export const loader = async () => {
 
 const UserProjects = () => {
   const { projects } = useLoaderData();
-  // const { updateProjectData } = useDashboardContext();
-
-  // useEffect(() => {
-  //   updateProjectData(null, null);
-  // }, []);
 
   return (
     <div className="relative flex flex-col flex-1">

bf7cc77be0b4c2bbf8e8c53eb50f420436543bad
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-07-08 08:49:18 +0300

    chore(assets): added images to use in the schema-visualizer

---
+++ src/features/schema-visualizer/assets/index.js
@@ -0,0 +1,7 @@
+export { default as externalLinkIcon } from "./externalLink.svg";
+export { default as isIdentityIcon } from "./identity.svg";
+export { default as isNotNullIcon } from "./notNullable.svg";
+export { default as isNullableIcon } from "./nullable.svg";
+export { default as isPrimaryKeyIcon } from "./primaryKey.svg";
+export { default as tableIcon } from "./table.svg";
+export { default as isUniqueIcon } from "./unique.svg";

f26e39c34ca0a5edb906d9aaac88d3554bad856b
omarsanad3 <omarmohamedsanad3@gmail.com>
2025-07-13 02:21:00 +0300

    fix(UI/dashboardLayout): fix the dashbaord not scrolling

--- src/pages/dashboard/DashboardLayout.jsx
+++ src/pages/dashboard/DashboardLayout.jsx
@@ -12,7 +12,8 @@ const DashboardLayout = () => {
         </div>
         <div>
           <Navbar />
-          <div className="flex flex-col border-gray-800 border-l-1 h-[calc(100vh-117px)] overflow-auto custom-scrollbar">
+          {/* <div className="flex flex-col border-gray-800 border-l-1 h-[calc(100vh-117px)] overflow-auto custom-scrollbar"> */}
+          <div className="flex flex-col border-gray-800 border-l-1 min-h-[calc(100vh-117px)]">
             <Outlet />
           </div>
         </div>
